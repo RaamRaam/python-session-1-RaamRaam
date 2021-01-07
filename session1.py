bd899b1954dec8cc09eef039f9f30329242897d4
Ramanathan Rm <rm.ramanathan@gmail.com>
2021-01-07 17:52:09 +0530

    initial commit

--- session1.py
+++ session1.py
@@ -1,12 +1,22 @@
-def myFunc():
+def myfunc():
     string='Cppsecrets'
     n=len(string)
     arr=[]
     for i in range(n):
-      for j in range(i+1,n+1):
-          a=string[i:j]
-          arr.append(a)
-	print(arr)
+        for j in range(i+1,n+1):
+            a=string[i:j]
+            arr.append(a)
+    print(arr)
 
-def my_Func():
+
+def my_func():
     pass
+
+
+class Rectangle:
+    def __init__(self, WIDTH, HEIGHT):
+        self.width = WIDTH
+        self.height = HEIGHT
+
+    def __repr__(self):
+        return f'Rectangle({self.width}, {self.height})'
